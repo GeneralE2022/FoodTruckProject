c2429a76dc2fad0363d05ea9ce966a5d0cf59414
Erich Schindler <erschindler@gmail.com>
2022-03-25 18:37:33 -0400

    1st commit

---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,10 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+	private static int totalTrucks = 0;
+	private int truckId;
+	private String truckName;
+	private String truckFoodType;
+	private int truckRating;
+
+}
